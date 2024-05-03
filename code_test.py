bd0e9b755d9e92b200d0f379703e0fae121254b8
JoshRobertson9 <rosh.jobertson@gmail.com>
2024-05-02 22:40:23 -0400

    Adding unit testing
    Starting on pytest integration

---
+++ code_test.py
@@ -0,0 +1,13 @@
+# Existing Modules
+import pytest
+import os
+
+# My Modules
+import home_functions as hf
+
+
+# Mortgage Rate Calc
+def test_mort_rate_calc():
+    assert round(hf.mortgage_rate_calc(400_000, 80_000, 6, 30),2) == 1918.56
+    #assert round(hf.mortgage_rate_calc(400_000, 80_000, 0.000000001, 30),2) == 888.89
+
